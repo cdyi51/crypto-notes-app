9b34790a532d2fad7e314ae97719e94abfbd1ee6
Christina Yi <89492759+cdyi51@users.noreply.github.com>
2023-10-06 05:17:55 -0400

    major changes

--- private_notes.py
+++ private_notes.py
@@ -1,52 +1,55 @@
 import pickle
 import os
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import hashes, padding, hmac
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from cryptography.hazmat.backends import default_backend
 
-encrypted_kvs = {}
 
 class PrivNotes:
   MAX_NOTE_LEN = 2048;
- 
+  
 
   def __init__(self, password, data = None, checksum = None):
     """Constructor.
     
     Args:
-      password (str) : password for accessing the notes
-      data (str) [Optional] : a hex-encoded serialized representation to load
-                              (defaults to None, which initializes an empty notes database)
-      checksum (str) [Optional] : a hex-encoded checksum used to protect the data against
-                                  possible rollback attacks (defaults to None, in which
-                                  case, no rollback protection is guaranteed)
+      password (str): password for accessing the notes
+      data (str) [Optional]: a hex-encoded serialized representation to load
+                             (defaults to None, which initializes an empty notes database)
+      checksum (str) [Optional]: a hex-encoded checksum used to protect the data against
+                                possible rollback attacks
 
     Raises:
-      ValueError : malformed serialized format
+      ValueError: malformed serialized format
     """
-    # first check if pw and checksum are correct
-        
-    #else...
-    
-    self.kvs = {} # initializing kvs to empty dictionary
     
     if data is not None:
-      self.kvs = pickle.loads(bytes.fromhex(data)) # loading the notes from data
+      # deserialize the data first to get the salt
+      deser_data = pickle.loads(bytes.fromhex(data))
+      [self.salt, self.nonces, self.kvs] = deser_data
 
     else:
-      """initialize empty database with pw as password""" 
-      # use salt to derive key with PBKDF2_HMAC
-      kdf = PBKDF2HMAC(algorithm = hashes.SHA256(), length = 32, salt = os.urandom(16), 
-                       iterations = 2000000) # add backend but idk what that really is
-      key = kdf.derive(bytes(password, 'ascii'))
-      
-     
-      # ct = aesgcm.encrypt(nonce, data, aad)
-      # aesgcm.decrypt(nonce, ct, aad)
-      
+      # if no data, initialize empty database and generate a new random salt
+      self.kvs = {}
+      self.nonces = {}
+      self.salt = os.urandom(16)
+        
+    # now derive the source key
+    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=self.salt, 
+                     iterations=2000000, backend=default_backend()
+    )
+    self.source_key = kdf.derive(bytes(password, 'ascii'))
+
+    # if data is provided, now verify the checksum
+    if data is not None:
+      h = hmac.HMAC(self.source_key, hashes.SHA256())
+      h.update(pickle.dumps([self.salt, self.nonces, self.kvs]))
+
+      if checksum != h.finalize().hex():
+          raise ValueError('Checksum is invalid, password is incorrect, or data has been tampered with.')
 
                                            
-
   def dump(self):
     """Computes a serialized representation of the notes database
        together with a checksum.
@@ -58,7 +61,11 @@ class PrivNotes:
                        against rollback attacks (up to 32 characters in length)
     """
     # return hexified data and checksum
-    return pickle.dumps(self.kvs).hex(), hashes.SHA256(self.kvs).hex()
+    # use HMAC not hash
+    # expand data to involve everything self needs to store
+    ser_data = [self.salt, self.nonces, self.kvs]
+    deser_data = pickle.dumps(ser_data).hex()
+    return deser_data, hmac.HMAC(self.source_key, hashes.SHA256()).hex()
 
   def get(self, title):
     """Fetches the note associated with a title.
@@ -70,9 +77,20 @@ class PrivNotes:
       note (str) : the note associated with the requested title if
                        it exists and otherwise None
     """
-    if title in self.kvs:
-      return self.kvs[title]
+    # first, pad and hmac title
+    padder = padding.PKCS7(16384).padder()
+    unpadder = padding.PKCS7(16384).unpadder()
+    hmacd_title = hmac.HMAC(padder.update(title) + padder.finalize(), hashes.SHA256())
+    if hmacd_title in self.nonces:
+      note = self.kvs[hmacd_title]
+      aesgcm = AESGCM(self.source_key)
+      decrypted_note = aesgcm.decrypt(self.nonces[hmacd_title], note, None)
+      unpadded_note = unpadder.update(decrypted_note) + unpadder.finalize()
+      # now change it to ASCII string
+      note = unpadded_note.decode('ascii')
+      return note
     return None
+
 
   def set(self, title, note):
     """Associates a note with a title and adds it to the database
@@ -89,22 +107,20 @@ class PrivNotes:
        Raises:
          ValueError : if note length exceeds the maximum
     """
-    # have to account for if adversary queries more than one note. if so, we have to set a b so that 
-    # the challenger (us) knows which one to actually encrypt/insert
-    
-    
     if len(note) > self.MAX_NOTE_LEN:
-      raise ValueError('Maximum note length exceeded')
-    nonce = os.urandom(12)
-      # encrypt the notes
-    for key in self.kvs:
-      unencrypted_note = self.kvs[key]
-      aesgcm = AESGCM(unencrypted_note)
-      # ig this is where I'd add the padding?
-      encrypted_note = aesgcm.encrypt(nonce, unencrypted_note, None)
-      encrypted_kvs[key] = encrypted_note
-      nonce += 1
-    # self.kvs[title] = note
+      raise ValueError('Maximum note length exceeded') 
+    padder = padding.PKCS7(16384).padder()
+    # Pad and hash the title, pad the note
+    hmacd_title = hmac.HMAC(padder.update(title) + padder.finalize(), hashes.SHA256())
+    padded_note = padder.update(note) + padder.finalize()
+    # encrypt the note and store the pair in kvs
+    aesgcm = AESGCM(self.source_key)
+    if hmacd_title in self.nonces:
+      nonce = self.nonces[hmacd_title]
+    else:
+      nonce = os.urandom(16)
+      self.nonces[hmacd_title] = nonce
+    self.kvs[hmacd_title] = aesgcm.encrypt(nonce, padded_note, None)
 
 
   def remove(self, title):
@@ -117,8 +133,11 @@ class PrivNotes:
          success (bool) : True if the title was removed and False if the title was
                           not found
     """
-    if title in self.kvs:
-      del self.kvs[title]
+    padder = padding.PKCS7(16384).padder()
+    hmacd_title = hmac.HMAC(padder.update(title) + padder.finalize(), hashes.SHA256())
+    if hmacd_title in self.nonces:
+      del self.kvs[hmacd_title]
       return True
-
+    # return false if hmaced_title is not in nonces, because this means 
+    # the title is not in kvs
     return False
